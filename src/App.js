64cdb03a8f54200d781a76c423e941edfd37d0c4
drakemaster <dartveider@abv.bg>
2018-11-25 14:42:19 +0100

    master - react app for displaying products in a table and fetching a single project through api

--- src/App.js
+++ src/App.js
@@ -1,28 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ProductList from './components/ProductList';
+import { Route, Switch } from "react-router";
+import { Link } from 'react-router-dom'
+import Product from './components/Product';
+import './styles/app.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <Header />
+        <main className="App-content">
+          <Switch>
+            <Route path="/" exact component={ProductList} />
+            <Route path="/product/:id" component={Product} />
+          </Switch>
+        </main>
+        <Footer />
+      </div >
     );
   }
 }
+
+
+const Header = () => {
+  return (
+    <header className="App-header">
+      <Link to="/"><h1>🦐 Exotic Fish Supplies</h1></Link>
+      <div className="right">
+        <button className="snipcart-checkout snipcart-summary">
+          <i className="fa fa-shopping-cart"></i> Checkout (<span className="snipcart-total-items"></span>)
+      </button>
+      </div>
+    </header>
+  );
+}
+
+const Footer = () => {
+  return (
+    <footer className="App-footer">
+      <div className="left">
+        Made by <a href="https://snipcart.com/blog" target="_blank" rel="noopener noreferrer">Snipcart</a> and ⚡ by <a href="https://strapi.io/">Strapi</a>
+      </div>
+      <div className="right">
+        <a href="https://github.com/snipcart/snipcart-strapi-react" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i> GitHub</a>
+        <a href="https://twitter.com/snipcart" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i> Twitter</a>
+      </div>
+    </footer>
+  );
+}
+
 
 export default App;
